fd5a58aa4e1eeeb71b7ecafec1319047e92e3a3f
AL-FAHAMI <altoihir@gmail.com>
2024-03-06 14:59:07 +0100

    contact manager project

---
+++ I_contactManager/ContactManager.java
@@ -0,0 +1,24 @@
+public class ContactManager {
+    private Contact[] contacts;
+
+    
+
+    public ContactManager(Contact[] contacts){
+        this.contacts = new Contact[contacts.length];
+        // iterate the array and do a deep copy of each element
+        for (int i = 0; i < this.contacts.length; i++) {
+            this.contacts[i] = contacts[i];
+        }
+    }
+
+    public Contact getContact(int index){
+        Contact copyContact = new Contact(this.contacts[index]);
+        return copyContact;
+    }
+
+    public void setContact(Contact contact, int index){
+        this.contacts[index] = new Contact(contact);
+    }
+
+    
+}
