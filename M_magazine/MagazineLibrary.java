5498d148c2251557025f006485a20988703b537c
AL-FAHAMI <altoihir@gmail.com>
2024-04-24 14:21:15 +0100

    magazine library

---
+++ M_magazine/MagazineLibrary.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class MagazineLibrary {
+    private ArrayList<Magazine> magazines;
+
+    public MagazineLibrary(){
+        magazines = new ArrayList<>();
+    }
+
+    public Magazine getMagazine(int index){
+        return new Magazine(this.magazines.get(index));
+    }
+
+    public void setMagazine(Magazine magazine, int index){
+
+        this.magazines.set(index, new Magazine(magazine));
+    }
+
+    public void addMagazine(Magazine magazine){
+        this.magazines.add(new Magazine(magazine));
+    }
+}
+
