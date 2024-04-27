1eb860139eae7496ea43c77197970268f8bcd374
AL-FAHAMI <altoihir@gmail.com>
2024-04-27 12:00:11 +0100

    starter game w constants

---
+++ O_starter_constants/constants/Regulation.java
@@ -0,0 +1,18 @@
+package constants;
+
+public class Regulation {
+
+    // constants go here
+    public final static int COURT_LENGTH = 94;
+    public final static int RIM_HEIGHT = 10;
+    public final static int THREE_POINT_DISTANCE = 23;
+    public final static int BEYOND_THREE_POINT_ARC = 3;
+    public final static int INSIDE_THREE_POINT_ARC = 2; 
+    public final static int FREE_THROW = 1;
+    public final static int QUARTER_LENGTH = 12;
+    public final static int POSSESSION_TIME = 24;
+
+
+}
+
+
