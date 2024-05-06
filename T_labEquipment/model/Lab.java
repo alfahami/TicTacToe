d5de2151b6ccc4ed9880b949e12db52c76345437
AL-FAHAMI <altoihir@gmail.com>
2024-05-06 22:55:40 +0100

    lab equipment polymorphisme

---
+++ T_labEquipment/model/Lab.java
@@ -0,0 +1,14 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Lab {
+
+    private ArrayList<LabEquipment> LabEquipments;
+
+    public Lab() {
+        this.LabEquipments = new ArrayList<>();
+    }
+
+    
+}
