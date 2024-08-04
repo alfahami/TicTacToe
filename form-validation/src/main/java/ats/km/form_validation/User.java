13c3d808fe60f2e0a3932d0d768a109f4e14721d
AL-FAHAMI <altoihir@gmail.com>
2024-08-04 23:47:28 +0100

    feat: add User POJO

---
+++ form-validation/src/main/java/ats/km/form_validation/User.java
@@ -0,0 +1,57 @@
+package ats.km.form_validation;
+
+import java.util.Date;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String email;
+    private Date dateOfBirth;
+
+
+    public User() {
+    }
+
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return this.userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDateOfBirth() {
+        return this.dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+}
